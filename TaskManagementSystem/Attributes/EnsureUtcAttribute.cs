1ec9898357bcf6e68f15782ed829402b60036591
Nikolay Zlatev <nikolay.zlatev@p3-group.com>
2024-11-27 10:09:39 +0200

    Introduced ensurer attribute to verify utc DateTime

---
+++ TaskManagementSystem/Attributes/EnsureUtcAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementSystem.Attributes;
+
+public sealed class EnsureUtcAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is DateTime dateTime)
+        {
+            if (dateTime.Kind != DateTimeKind.Utc)
+            {
+                return new ValidationResult("The date must be in UTC.");
+            }
+
+            if (dateTime <= DateTime.UtcNow)
+            {
+                return new ValidationResult("The date must be in the future");
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
