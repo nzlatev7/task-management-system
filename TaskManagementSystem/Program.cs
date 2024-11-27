23fd6efa0526a481ed00c370873f9ce12766239c
Nikolay Zlatev <nikolay.zlatev@p3-group.com>
2024-11-27 22:52:48 +0200

    registered services to DI

--- TaskManagementSystem/Program.cs
+++ TaskManagementSystem/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 using TaskManagementSystem;
+using TaskManagementSystem.Interfaces;
+using TaskManagementSystem.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,9 @@ builder.Services.AddOpenApi();
 // configure db provider
 var dbConnectionString = builder.Configuration.GetConnectionString("Database");
 builder.Services.AddDbContext<TaskManagementSystemDbContext>(opt => opt.UseNpgsql(dbConnectionString));
+
+builder.Services.AddScoped<ITasksService, TasksService>();
+builder.Services.AddScoped<ICategoriesService, CategoriesSerivce>();
 
 var app = builder.Build();
 
