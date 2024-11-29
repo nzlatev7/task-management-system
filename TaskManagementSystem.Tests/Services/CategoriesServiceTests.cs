b7df5572a2286228fff391197cad49ffd5f515b9
Nikolay Zlatev <nikolay.zlatev@p3-group.com>
2024-11-29 14:50:43 +0200

    Added tests for the Controllers and Services

---
+++ TaskManagementSystem.Tests/Services/CategoriesServiceTests.cs
@@ -0,0 +1,268 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagementSystem.DTOs.Request;
+using TaskManagementSystem.Services;
+using TaskManagementSystem.Tests.TestUtilities;
+
+namespace TaskManagementSystem.Tests.Services;
+
+public sealed class CategoriesServiceTests : IDisposable
+{
+    private readonly TaskManagementSystemDbContext _dbContext;
+    private readonly TestDataGenerator _dataGenerator;
+    private readonly CategoriesSerivce _categoriesService;
+
+    public CategoriesServiceTests()
+    {
+        var options = new DbContextOptionsBuilder<TaskManagementSystemDbContext>()
+            .UseInMemoryDatabase("TestDb-CategoriesServiceTests")
+            .Options;
+
+        _dbContext = new TaskManagementSystemDbContext(options);
+        _dataGenerator = new TestDataGenerator(_dbContext);
+        _categoriesService = new CategoriesSerivce(_dbContext);
+    }
+
+    public void Dispose()
+    {
+        _dbContext.Database.EnsureDeleted();
+        _dbContext.Dispose();
+    }
+
+    #region CreateCategory
+
+    [Fact]
+    public async Task CreateCategoryAsync_CategoryCreated_ReturnsTheCategory()
+    {
+        // Arrange
+        var categoryDto = new CategoryRequestDto
+        {
+            Name = "Test",
+            Description = "Test Description",
+        };
+
+        // Act
+        var result = await _categoriesService.CreateCategoryAsync(categoryDto);
+
+        // Assert            
+        Assert.Equal(categoryDto.Name, result.Name);
+        Assert.Equal(categoryDto.Description, result.Description);
+
+        var savedCategory = await _dbContext.Categories
+            .FirstOrDefaultAsync(t => t.Id == result.Id);
+
+        Assert.NotNull(savedCategory);
+        Assert.Equal(categoryDto.Name, savedCategory.Name);
+        Assert.Equal(categoryDto.Description, savedCategory.Description);
+    }
+
+    #endregion
+
+    #region GetAllCategories
+
+    [Fact]
+    public async Task GetAllCategoriesAsync_ReturnsAllCategories()
+    {
+        // Arrange
+        var categories = await _dataGenerator.InsertCategories(count: 2);
+
+        // Act
+        var result = await _categoriesService.GetAllCategoriesAsync();
+
+        // Assert            
+        var expectedResult = TestResultBuilder.GetExpectedCategories(categories);
+        Assert.Equivalent(expectedResult, result, strict: true);
+    }
+
+    #endregion
+
+    #region GetCategoryById
+
+    [Fact]
+    public async Task GetCategoryByIdAsync_ReturnsExpectedCategory()
+    {
+        // Arrange
+        var categories = await _dataGenerator.InsertCategories(count: 2);
+        var targetCategory = categories[0];
+
+        // Act
+        var result = await _categoriesService.GetCategoryByIdAsync(targetCategory.Id);
+
+        // Assert            
+        Assert.NotNull(result);
+        Assert.Equal(targetCategory.Name, result.Name);
+        Assert.Equal(targetCategory.Description, result.Description);
+    }
+
+    [Fact]
+    public async Task GetCategoryByIdAsync_CategoryDoesNotExist_ReturnsNull()
+    {
+        // Arrange
+        var nonExistingCategoryId = 1000;
+        var categories = await _dataGenerator.InsertCategories(count: 2);
+
+        // Act
+        var result = await _categoriesService.GetCategoryByIdAsync(nonExistingCategoryId);
+
+        // Assert            
+        Assert.Null(result);
+    }
+
+    #endregion
+
+    #region UdateCategory
+
+    [Fact]
+    public async Task UdateCategoryAsync_CategoryExists_ReturnsUpdatedCategory()
+    {
+        // Arrange
+        var categories = await _dataGenerator.InsertCategories(count: 2);
+        var targetCategory = categories[0];
+
+        var categoryDto = new CategoryRequestDto
+        {
+            Name = "Updated",
+            Description = targetCategory.Description,
+        };
+
+        // Act
+        var result = await _categoriesService.UpdateCategoryAsync(targetCategory.Id, categoryDto);
+
+        // Assert            
+        Assert.NotNull(result);
+        Assert.Equal(targetCategory.Name, result.Name);
+        Assert.Equal(targetCategory.Description, result.Description);
+
+        var updatedCategory = await _dbContext.Categories
+            .FirstOrDefaultAsync(t => t.Id == result.Id);
+
+        Assert.NotNull(updatedCategory);
+        Assert.Equal(categoryDto.Name, updatedCategory.Name);
+        Assert.Equal(categoryDto.Description, updatedCategory.Description);
+    }
+
+    [Fact]
+    public async Task UdateCategoryAsync_CategoryDoesNotExist_ReturnsNull()
+    {
+        // Arrange
+        var nonExistingCategoryId = 1000;
+        var categories = await _dataGenerator.InsertCategories(count: 2);
+
+        var categoryDto = new CategoryRequestDto
+        {
+            Name = "Updated",
+            Description = nameof(CategoryRequestDto.Description),
+        };
+
+        // Act
+        var result = await _categoriesService.UpdateCategoryAsync(nonExistingCategoryId, categoryDto);
+
+        // Assert            
+        Assert.Null(result);
+    }
+
+    #endregion
+
+    #region DeleteCategory
+
+    [Fact]
+    public async Task DeleteCategoryAsync_CategoryExists_CategoryDeleted_ReturnsTrue()
+    {
+        // Arrange
+        var categories = await _dataGenerator.InsertCategories(count: 2);
+        var targetCategoryId = categories[0].Id;
+
+        // Act
+        var result = await _categoriesService.DeleteCategoryAsync(targetCategoryId);
+
+        // Assert            
+        Assert.True(result);
+
+        var count = await _dbContext.Categories.CountAsync();
+        var expectedCount = categories.Count - 1;
+
+        Assert.Equal(expectedCount, count);
+
+        var deletedCategory = await _dbContext.Categories
+            .FirstOrDefaultAsync(x => x.Id == targetCategoryId);
+
+        Assert.Null(deletedCategory);
+    }
+
+    [Fact]
+    public async Task DeleteCategoryAsync_CategoryDoesNotExist_ReturnsNull()
+    {
+        // Arrange
+        var nonExistingCategoryId = 1000;
+        var categories = await _dataGenerator.InsertCategories(count: 2);
+
+        var categoryDto = new CategoryRequestDto
+        {
+            Name = "Updated",
+            Description = nameof(CategoryRequestDto.Description),
+        };
+
+        // Act
+        var result = await _categoriesService.DeleteCategoryAsync(nonExistingCategoryId);
+
+        // Assert            
+        Assert.False(result);
+
+        var count = await _dbContext.Categories.CountAsync();
+        Assert.Equal(categories.Count, count);
+    }
+
+    #endregion
+
+    #region GetTasksByCategory
+
+    [Fact]
+    public async Task GetTasksByCategoryAsync_ReturnsTasksForThisCategory()
+    {
+        // Arrange
+        var categories = await _dataGenerator.InsertCategories(count: 2);
+        var targetCategoryId = categories[0].Id;
+
+        var targetTasks = await _dataGenerator.InsertTasks(count: 2, targetCategoryId);
+        await _dataGenerator.InsertTasks(count: 2, categoryId: categories[1].Id);
+
+        // Act
+        var result = await _categoriesService.GetTasksByCategoryAsync(targetCategoryId);
+
+        // Assert            
+        var expectedResult = TestResultBuilder.GetExpectedTasks(targetTasks);
+        Assert.Equivalent(expectedResult, result);
+    }
+
+    #endregion
+
+    #region CategoryExists
+
+    [Fact]
+    public async Task CategoryExistsAsync_CategoryExists_ReturnsTrue()
+    {
+        // Arrange
+        var categories = await _dataGenerator.InsertCategories(count: 1);
+        var targetCategoryId = categories[0].Id;
+
+        // Act
+        var result = await _categoriesService.CategoryExistsAsync(targetCategoryId);
+
+        // Assert            
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task CategoryExistsAsync_CategoryDoesNotExist_ReturnsFalse()
+    {
+        // Arrange
+        var categories = await _dataGenerator.InsertCategories(count: 1);
+
+        // Act
+        var result = await _categoriesService.CategoryExistsAsync(categoryId: 9999);
+
+        // Assert            
+        Assert.False(result);
+    }
+
+    #endregion
+}
---
+++ TaskManagementSystem.Tests/Services/TasksServiceTests.cs
@@ -0,0 +1,294 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using TaskManagementSystem.Constants;
+using TaskManagementSystem.DTOs.Request;
+using TaskManagementSystem.Interfaces;
+using TaskManagementSystem.Services;
+using TaskManagementSystem.Tests.TestUtilities;
+
+namespace TaskManagementSystem.Tests.Services;
+
+public sealed class TasksServiceTests : IDisposable
+{
+    private readonly TaskManagementSystemDbContext _dbContext;
+    private readonly Mock<ICategoriesService> _categoriesServiceMock;
+    private readonly TestDataGenerator _dataGenerator;
+
+    private readonly TasksService _tasksService;
+
+    public TasksServiceTests()
+    {
+        var options = new DbContextOptionsBuilder<TaskManagementSystemDbContext>()
+            .UseInMemoryDatabase("TestDb-TasksServiceTests")
+            .Options;
+
+        _dbContext = new TaskManagementSystemDbContext(options);
+        _dataGenerator = new TestDataGenerator(_dbContext);
+
+        _categoriesServiceMock = new Mock<ICategoriesService>();
+        _tasksService = new TasksService(_dbContext, _categoriesServiceMock.Object);
+    }
+
+    public void Dispose()
+    {
+        _dbContext.Database.EnsureDeleted();
+        _dbContext.Dispose();
+    }
+
+    #region CreateTask
+
+    [Fact]
+    public async Task CreateTaskAsync_CategoryExists_TaskCreated_ReturnsTask()
+    {
+        // Arrange
+        var taskDto = new TaskRequestDto
+        {
+            Title = "New Task",
+            Description = "Task description",
+            DueDate = DateTime.UtcNow.AddDays(1),
+            IsCompleted = false,
+            CategoryId = 1
+        };
+
+        _categoriesServiceMock.Setup(c => c.CategoryExistsAsync(taskDto.CategoryId))
+            .ReturnsAsync(true);
+
+        // Act
+        var result = await _tasksService.CreateTaskAsync(taskDto);
+
+        // Assert
+        Assert.Equal(taskDto.Title, result.Title);
+        Assert.Equal(taskDto.Description, result.Description);
+        Assert.Equal(taskDto.DueDate, result.DueDate);
+        Assert.Equal(taskDto.IsCompleted, result.IsCompleted);
+
+        var savedTask = await _dbContext.Tasks
+            .FirstOrDefaultAsync(t => t.Id == result.Id);
+
+        Assert.NotNull(savedTask);
+        Assert.Equal(taskDto.Title, savedTask.Title);
+        Assert.Equal(taskDto.Description, savedTask.Description);
+        Assert.Equal(taskDto.DueDate, savedTask.DueDate);
+        Assert.Equal(taskDto.IsCompleted, savedTask.IsCompleted);
+
+        _categoriesServiceMock.Verify(c => c.CategoryExistsAsync(It.IsAny<int>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task CreateTaskAsync_CategoryDoesNotExist_ThrowsArgumentException()
+    {
+        // Arrange
+        var taskDto = new TaskRequestDto
+        {
+            Title = "New Task",
+            Description = "Task description",
+            DueDate = DateTime.UtcNow.AddDays(1),
+            IsCompleted = false,
+            CategoryId = 10000
+        };
+
+        _categoriesServiceMock.Setup(c => c.CategoryExistsAsync(taskDto.CategoryId))
+            .ReturnsAsync(false);
+
+        // Act & Assert
+        var exception = await Assert.ThrowsAsync<ArgumentException>(() => _tasksService.CreateTaskAsync(taskDto));
+        Assert.Equal(ValidationMessages.CategoryDoesNotExist, exception.Message);
+    }
+
+    #endregion
+
+    #region GetAllTasks
+
+    [Fact]
+    public async Task GetAllTasksAsync_ReturnsAllTasks()
+    {
+        // Arrange
+        var tasks = await _dataGenerator.InsertTasks(count: 2, categoryId: 1);
+
+        // Act
+        var result = await _tasksService.GetAllTasksAsync();
+
+        // Assert
+        var expectedTasks = TestResultBuilder.GetExpectedTasks(tasks);
+        Assert.Equivalent(expectedTasks, result, strict: true);
+    }
+
+    #endregion
+
+    #region GetTaskById
+
+    [Fact]
+    public async Task GetTaskByIdAsync_ReturnsExpectedTask()
+    {
+        // Arrange
+        var tasks = await _dataGenerator.InsertTasks(count: 2, categoryId: 1);
+        var expectedTask = tasks[0];
+
+        // Act
+        var result = await _tasksService.GetTaskByIdAsync(expectedTask.Id);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(expectedTask.Id, result.Id);
+        Assert.Equal(expectedTask.Title, result.Title);
+        Assert.Equal(expectedTask.Description, result.Description);
+        Assert.Equal(expectedTask.DueDate, result.DueDate);
+        Assert.Equal(expectedTask.IsCompleted, result.IsCompleted);
+    }
+
+    [Fact]
+    public async Task GetTaskByIdAsync_TaskDoesNotExist_ReturnsNull()
+    {
+        // Arrange
+        var tasks = await _dataGenerator.InsertTasks(count: 2, categoryId: 1);
+
+        // Act
+        var result = await _tasksService.GetTaskByIdAsync(taskId: 1000);
+
+        // Assert
+        Assert.Null(result);
+    }
+
+    #endregion
+
+    #region UpdateTask
+
+    [Fact]
+    public async Task UpdateTaskAsync_TaskExists_CategoryExists_TaskUpdated_ReturnsTask()
+    {
+        // Arrange
+        var tasks = await _dataGenerator.InsertTasks(count: 2, categoryId: 1);
+        var targetTask = tasks[0];
+
+        targetTask.Title = "Updated Title";
+
+        var taskDto = new TaskRequestDto()
+        {
+            Title = targetTask.Title,
+            Description = targetTask.Description,
+            DueDate = targetTask.DueDate,
+            IsCompleted = targetTask.IsCompleted,
+        };
+
+        _categoriesServiceMock.Setup(c => c.CategoryExistsAsync(taskDto.CategoryId))
+            .ReturnsAsync(true);
+
+        // Act
+        var result = await _tasksService.UpdateTaskAsync(targetTask.Id, taskDto);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(targetTask.Id, result.Id);
+        Assert.Equal(targetTask.Title, result.Title);
+        Assert.Equal(targetTask.Description, result.Description);
+        Assert.Equal(targetTask.DueDate, result.DueDate);
+        Assert.Equal(targetTask.IsCompleted, result.IsCompleted);
+
+        var updatedTask = await _dbContext.Tasks
+            .FirstOrDefaultAsync(t => t.Id == result.Id);
+
+        Assert.NotNull(updatedTask);
+        Assert.Equal(taskDto.Title, updatedTask.Title);
+        Assert.Equal(taskDto.Description, updatedTask.Description);
+        Assert.Equal(taskDto.DueDate, updatedTask.DueDate);
+        Assert.Equal(taskDto.IsCompleted, updatedTask.IsCompleted);
+
+        _categoriesServiceMock.Verify(c => c.CategoryExistsAsync(It.IsAny<int>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task UpdateTaskAsync_TaskDoesNotExist_ReturnsNull()
+    {
+        // Arrange
+        var nonExistingTaskId = 1000;
+
+        var tasks = await _dataGenerator.InsertTasks(count: 2, categoryId: 1);
+        var targetTask = tasks[0];
+
+        targetTask.Title = "Updated Title";
+
+        var taskDto = new TaskRequestDto()
+        {
+            Title = targetTask.Title,
+            Description = targetTask.Description,
+            DueDate = targetTask.DueDate,
+            IsCompleted = targetTask.IsCompleted,
+        };
+
+        // Act
+        var result = await _tasksService.UpdateTaskAsync(nonExistingTaskId, taskDto);
+
+        // Assert
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public async Task UpdateTaskAsync_TaskExists_CategoryDoesNotExist_ThrowsArgumentException()
+    {
+        // Arrange
+        var tasks = await _dataGenerator.InsertTasks(count: 2, categoryId: 1);
+        var targetTask = tasks[0];
+
+        targetTask.Title = "Updated Title";
+
+        var taskDto = new TaskRequestDto()
+        {
+            Title = targetTask.Title,
+            Description = targetTask.Description,
+            DueDate = targetTask.DueDate,
+            IsCompleted = targetTask.IsCompleted,
+        };
+
+        _categoriesServiceMock.Setup(c => c.CategoryExistsAsync(taskDto.CategoryId))
+            .ReturnsAsync(false);
+
+        // Act & Assert
+        var exception = await Assert.ThrowsAsync<ArgumentException>(() => _tasksService.UpdateTaskAsync(targetTask.Id, taskDto));
+        Assert.Equal(ValidationMessages.CategoryDoesNotExist, exception.Message);
+    }
+
+    #endregion
+
+    #region DeleteTask
+
+    [Fact]
+    public async Task DeleteTaskAsync_TaskExists_TaskDeleted_ReturnsTrue()
+    {
+        // Arrange
+        var tasks = await _dataGenerator.InsertTasks(count: 2, categoryId: 1);
+        var targetTaskId = tasks[0].Id;
+
+        // Act
+        var result = await _tasksService.DeleteTaskAsync(targetTaskId);
+
+        // Assert
+        Assert.True(result);
+
+        var count = _dbContext.Tasks.Count();
+        var expectedCount = tasks.Count - 1;
+        Assert.Equal(expectedCount, count);
+
+        var deletedTask = await _dbContext.Tasks.FirstOrDefaultAsync(x => x.Id == targetTaskId);
+        Assert.Null(deletedTask);
+    }
+
+    [Fact]
+    public async Task DeleteTaskAsync_TaskDoesNotExist_ReturnsFalse()
+    {
+        // Arrange
+        var tasks = await _dataGenerator.InsertTasks(count: 2, categoryId: 1);
+
+        // Act
+        var result = await _tasksService.DeleteTaskAsync(taskId: 1000);
+
+        // Assert
+        Assert.False(result);
+
+        var count = _dbContext.Tasks.Count();
+        Assert.Equal(tasks.Count, count);
+    }
+
+    #endregion
+
+
+}
