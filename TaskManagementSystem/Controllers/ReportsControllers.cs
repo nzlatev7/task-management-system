b346bf9bd007089db5a01774f71910d48dd9dca1
Nikolay Zlatev <nikolay.zlatev@p3-group.com>
2024-12-06 09:57:26 +0200

    Introduced reporting endpoint for tasks

---
+++ TaskManagementSystem/Controllers/ReportsControllers.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagementSystem.DTOs.Request;
+using TaskManagementSystem.DTOs.Response;
+using TaskManagementSystem.Interfaces;
+
+namespace TaskManagementSystem.Controllers;
+
+[ApiController]
+[Route("api/reports")]
+public class ReportsControllers : ControllerBase
+{
+    private readonly IReportsService _reportsService;
+    public ReportsControllers(IReportsService reportsService)
+    {
+        _reportsService = reportsService;
+    }
+
+    [HttpGet("tasks")]
+    public async Task<ActionResult<IEnumerable<ReportTasksResponseDto>>> GetReportForTasks([FromQuery] ReportTasksRequestDto reportDto)
+    {
+        var result = await _reportsService.GetReportForTasksAsync(reportDto);
+
+        return Ok(result);
+    }
+}
---
+++ TaskManagementSystem/Services/ReportsService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagementSystem.Database;
+using TaskManagementSystem.Database.Models;
+using TaskManagementSystem.DTOs.Request;
+using TaskManagementSystem.DTOs.Response;
+using TaskManagementSystem.Helpers;
+using TaskManagementSystem.Interfaces;
+
+namespace TaskManagementSystem.Services;
+
+public class ReportsService : IReportsService
+{
+    private readonly TaskManagementSystemDbContext _dbContext;
+    public ReportsService(TaskManagementSystemDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<IEnumerable<ReportTasksResponseDto>> GetReportForTasksAsync(ReportTasksRequestDto reportDto)
+    {
+        IQueryable<TaskEntity> tasks = _dbContext.Tasks;
+
+        tasks = ApplyFilters(tasks, reportDto);
+
+        var result = await tasks
+            .GroupBy(x => x.CategoryId)
+            .Select(g => new ReportTasksResponseDto()
+            {
+                CategoryId = g.Key,
+                Tasks = g.Select(x => x.ToOutDto())
+                    .ToList()
+            })
+            .ToListAsync();
+
+        return result;
+    }
+
+    private IQueryable<TaskEntity> ApplyFilters(IQueryable<TaskEntity> tasks, ReportTasksRequestDto reportDto)
+    {
+        if (reportDto.Status.HasValue)
+            tasks = tasks.Where(x => x.Status == reportDto.Status);
+
+        if (reportDto.Priority.HasValue)
+            tasks = tasks.Where(x => x.Priority == reportDto.Priority);
+
+        if (reportDto.DueBefore.HasValue && reportDto.DueAfter.HasValue)
+            tasks = tasks.Where(x => x.DueDate < reportDto.DueBefore || x.DueDate > reportDto.DueAfter);
+        else if (reportDto.DueBefore.HasValue)
+            tasks = tasks.Where(x => x.DueDate < reportDto.DueBefore);
+        else if (reportDto.DueAfter.HasValue)
+            tasks = tasks.Where(x => x.DueDate > reportDto.DueBefore);
+
+        return tasks;
+    }
+}
